1cdb5fe9857e9e64400fef247413318144d9de7f
jrybojad <jrybojad@ucsc.edu>
2019-02-28 12:53:32 -0800

    log file working

--- exchange_server_116/log.py
+++ exchange_server_116/log.py
@@ -15,10 +15,10 @@ now = datetime.datetime.now()
 fileName = userID + '_' + str(now)[:10] + '.csv'
 # writing data to a csv file to record the history of orders
 print(fileName)
-with open(fileName, mode = 'w') as order_history:
+with open(fileName, mode = 'a') as order_history:
     myFields = ['order_ID', 'status', 'direction', 'time_in_force', 'timestamp',
                 'stock_price', 'stock_quantity', 'trader_cash', 'current_stock']
-    writer = csv.DictWriter(order_history, fieldnames=myFields)
-    writer.writeheader()
-    writer.writerow({userID, 'confirmed', 'B', 99999, 23903432342, 3333, 222, 2232, 25} )
+    writer = csv.writer(order_history)
+    writer.writerow(myFields)
+    writer.writerow([userID, 'unconfirmed', 'B', 99999, 384398343, 3333, 222, 1500, 120])
     order_history.close()
--- exchange_server_116/xml_station.py
+++ exchange_server_116/xml_station.py
@@ -169,16 +169,11 @@ class CSVManager:
 
 
 def main():
-<<<<<<< HEAD
-    userID
-    user = Trade_Station(1000000, userID)
-=======
 
     userID = '0001'
     user = Trade_Station(1000000, userID)
     now = datetime.datetime.now()
     fileName = userID + '_' + str(now.year) + '_' + str(now.month) + '_' + str(now.day) + '.csv'
->>>>>>> 07d8fbe27d28e29cf44353ff03d08b65c6921095
     # writing data to a csv file to record the history of orders
 
 
