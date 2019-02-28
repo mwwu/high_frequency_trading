251f60a2cea289b3a2ee1f1f4f77a15b86f42398
jrybojad <jrybojad@ucsc.edu>
2019-02-27 20:49:10 -0800

    log.py working

--- exchange_server_116/log.py
+++ exchange_server_116/log.py
@@ -7,16 +7,17 @@ import time
 import numpy
 # writing to csv file
 import csv
+import datetime
 
-def main():
-    userID = '0001'
-    user = Trade_Station(1000000, userID)
-    now = datetime.datetime.now()
-    fileName = userID + '_' + str(now.year) + '_' + str(now.month) + '_' + str(now.day) + '.csv'
-    # writing data to a csv file to record the history of orders
-    history = open(fileName, 'w')
-    with history:
-        myFields = ['trader_ID', 'status', 'direction', 'time_in_force', 'timestamp',
-                    'stock_price', 'stock_quantity', 'trader_cash', 'current_stock']
-        writer = csv.DictWriter(fileName, fieldnames=myFields)
-        writer.writeheader()
+
+userID = '0001'
+now = datetime.datetime.now()
+fileName = userID + '_' + str(now)[:10] + '.csv'
+# writing data to a csv file to record the history of orders
+print(fileName)
+with open(fileName, mode = 'w') as order_history:
+    myFields = ['trader_ID', 'status', 'direction', 'time_in_force', 'timestamp',
+                'stock_price', 'stock_quantity', 'trader_cash', 'current_stock']
+    writer = csv.DictWriter(order_history, fieldnames=myFields)
+    writer.writeheader()
+    order_history.close()
