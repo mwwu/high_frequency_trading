cecd000424f8ecffd63affd2a94e3a8b2656684a
jrybojad <jrybojad@ucsc.edu>
2019-02-27 20:56:31 -0800

    log.py working

--- exchange_server_116/log.py
+++ exchange_server_116/log.py
@@ -16,8 +16,9 @@ fileName = userID + '_' + str(now)[:10] + '.csv'
 # writing data to a csv file to record the history of orders
 print(fileName)
 with open(fileName, mode = 'w') as order_history:
-    myFields = ['trader_ID', 'status', 'direction', 'time_in_force', 'timestamp',
+    myFields = ['order_ID', 'status', 'direction', 'time_in_force', 'timestamp',
                 'stock_price', 'stock_quantity', 'trader_cash', 'current_stock']
     writer = csv.DictWriter(order_history, fieldnames=myFields)
     writer.writeheader()
+    writer.writerow({userID, 'confirmed', 'B', 99999, 23903432342, 3333, 222, 2232, 25} )
     order_history.close()
