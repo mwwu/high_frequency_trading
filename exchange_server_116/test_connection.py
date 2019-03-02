07d7689e5d86a54d6121f699c648eea6594d100e
Fbraga <fbraga36@gmail.com>
2019-03-01 16:05:55 -0800

    Updated maker_robot and test_connection

--- exchange_server_116/test_connection.py
+++ exchange_server_116/test_connection.py
@@ -1,38 +1,7 @@
 
 import maker_robot
-from __future__ import print_function
 
-from twisted.internet import task
-from twisted.internet.defer import Deferred
-from twisted.internet.protocol import ClientFactory
-from twisted.protocols.basic import LineReceiver
-
-class EchoClient(LineReceiver):
-    def connectionMade(self):
-        # ordertoken, buy/sell, stock, price range
-        # sends message to server
-        self.sendLine(b"00000000000000:B44xAMAZGOOG@33")
-
-    def lineReceived(self, line):
-        # prints to client
-        print("sent:", line)
-
-class EchoClientFactory(ClientFactory):
-    protocol = EchoClient
-
-    def __init__(self):
-        self.done = Deferred()
-
-    def clientConnectionFailed(self, connector, reason):
-        print('connection failed:', reason.getErrorMessage())
-        self.done.errback(reason)
-
-    def clientConnectionLost(self, connector, reason):
-        print('connection lost:', reason.getErrorMessage())
-        self.done.callback(None)
-
-
-def main(reactor):
+def main():
     #roboti = Maker_Clinet(Cash, id)
-    robot1 = Maker_Client(0, 1)
-    robot2 = Maker_Clinet(0, 2)
+    robot1 = maker_robot.Maker_Client(0, 1)
+
