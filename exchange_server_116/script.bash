905616cab9360626628858d2da749f42d033000f
minhanhn98 <minhanhn98@gmail.com>
2019-04-18 17:24:49 -0700

    added dependencies

--- exchange_server_116/script.bash
+++ exchange_server_116/script.bash
@@ -1,6 +1,10 @@
+source run_exchange_server.py
+source test_broker.py
+source test_trader_client.py
+source random_trader_client.py
+
 xterm -hold -e python run_exchange_server.py --host 0.0.0.0 --port 9001 --debug --mechanism cda &
 xterm -hold -e python test_broker.py &
 xterm -hold -e python test_trader_client.py &
-xterm -hold -e python random_trader_client.py &
-xterm -hold -e "echo Hello My World"
+xterm -hold -e python random_trader_client.py 
 
