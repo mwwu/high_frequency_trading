e8c97f99318c7eb9b6933d59c315ee9861c896a6
Fbraga <fbraga36@gmail.com>
2019-05-17 09:26:59 -0700

    Removed unecessary line from script

--- exchange_server_cs/script.bash
+++ exchange_server_cs/script.bash
@@ -1,5 +1,4 @@
 xterm -hold -e python3 run_exchange_server.py --host 0.0.0.0 --port 9001 --debug --mechanism cda &
 xterm -hold -e python3 broker.py &
-xterm -hold -e python3 external_clients.py &
-xterm -hold -e python3 both_market_external_clients.py 
+xterm -hold -e python3 external_clients.py
 
